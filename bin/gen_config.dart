546157bf92335be811ea8afbb7e51a84c6a17ab2
Hau Tran <hautv@vuihoc.vn>
2024-08-04 00:40:50 +0700

    [HauTV] Update drinking card 5

--- bin/gen_config.dart
+++ bin/gen_config.dart
@@ -5,7 +5,7 @@ import 'utils/branch_utils.dart';
 
 const secretEnv = {
   "spreadsheets_id": "1rdaPpBKf9zMKFIDCiDClFi_edYLOexKMCgFncAuRwyQ",
-  "sheet_name": ["list_pack", "1", "2", "3"],
+  "sheet_name": ["list_pack", "1", "2", "5", "3"],
 };
 void main(List<String> args) async {
   final branchName = await BranchUtil.branch;
